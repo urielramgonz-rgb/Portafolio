f00d96e4a45f730ee865f00fd922c0ba649d98c6
urielramgonz-rgb <urielramgonz@gmail.com>
2026-09-24 15:52:59 -0600

    Saludo Nombre

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -2,18 +2,18 @@ package datos.unidad1.recursividad;
 
 public class Recursividad {
 
-	public static void saludo(int total){
+	public static void saludo(int total, String nombre){
 
 	if (total <= 0) //Condicion base
 		return;
 	else{ //Condicion recursiva o repetitiva
-		System.out.println("Hola");
-		saludo(total-1);
+		System.out.println("Hola" + nombre);
+		saludo(total-1, nombre);
 		}		
 	}
 
 	public static void main (String[] args ){
-	saludo(10);	
+	saludo(10, " Uriel");	
 
 	}
 
