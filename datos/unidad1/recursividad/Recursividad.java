9b53f8f8b4d0b076b1854386f41223a31188fd48
urielramgonz-rgb <urielramgonz@gmail.com>
2026-09-25 13:58:12 -0600

    Agruegue funcion de cuenta regresiva y esta super cool

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -7,13 +7,31 @@ public class Recursividad {
 	if (total <= 0) //Condicion base
 		return;
 	else{ //Condicion recursiva o repetitiva
-		System.out.println("Hola" + nombre);
+		System.out.println("Hola " + nombre);
 		saludo(total-1, nombre);
 		}		
 	}
+	/**
+	*Funcion que realiza cuenta regresiva
+	*Valor entero
+	*Fecha: 25 de septiembre
+	*Autor: Uriel Ramírez
+	*/
+
+	public static void cuentaRegresiva(int n){
+	
+	if(n<0){
+		return;
+	
+	} else{
+		System.out.println(n + " ");
+		cuentaRegresiva(n-1);
+	}
+}
 
 	public static void main (String[] args ){
-	saludo(10, " Uriel");	
+		//saludo(10, "Uriel");
+		cuentaRegresiva(100);
 
 	}
 
